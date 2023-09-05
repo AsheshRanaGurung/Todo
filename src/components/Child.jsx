59606bcea8f32c25b09e7bf773a1edc667e3579b
AsheshRanaGurung <ashesh7272@gmail.com>
2023-09-05 06:23:12 +0545

    feat: componennts

---
+++ src/components/Child.jsx
@@ -0,0 +1,13 @@
+import { Input } from "@chakra-ui/react";
+import React from "react";
+
+const InputForm = () => {
+  return (
+    <>
+      <div>InputForm</div>
+      <Input placeholder="Basic usage" />
+    </>
+  );
+};
+
+export default InputForm;
