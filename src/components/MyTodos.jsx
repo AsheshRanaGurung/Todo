3bc4357bc40cd98b005301154fde10327c5bde48
AsheshRanaGurung <ashesh7272@gmail.com>
2023-09-06 07:27:39 +0545

    fix: add

--- src/components/MyTodos.jsx
+++ src/components/MyTodos.jsx
@@ -1,13 +1,22 @@
 import { Box, Text } from "@chakra-ui/react";
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 
 const MyTodos = (props) => {
   const myArr = props.todos ?? [];
   return myArr.map((obj) => {
     return (
-      <Box>
-        <Text>{obj.text}</Text>
-      </Box>
+      <AnimatePresence key={obj.id}>
+        <Box
+          as={motion.div}
+          layoutId={obj.id}
+          initial={{ scale: 0 }}
+          exit={{ scale: 0 }}
+          animate={{ scale: 0.9 }}
+        >
+          <Text>{obj.text}</Text>
+        </Box>
+      </AnimatePresence>
     );
   });
 };
